01fdf56f0b49f5cf193cbc367e9772a1966469ec
gaswa <89263671+gaswa@users.noreply.github.com>
2021-08-21 09:21:58 +0530

    Add files via upload

---
+++ first website/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+   $(window).scroll(function(){
+      var scroll = $(window).scrollTop();
+      if (scroll > 300) {
+        $(".black").css("background" , "red");
+      }
+ 
+      else{
+         $(".black").css("background" , "grey");  	
+      }
+   })
+ })   
+ 
+ 
